9deb34436d8a2a7acd15d7e1dad3eaf5721b0abc
Cristiano Rodrigues Borges <cristiano.borges@cwi.com.br>
2020-12-21 19:28:29 -0300

    Antes do bug do mat card

---
+++ Projetocrborges/frontend/src/app/app.module.ts
@@ -0,0 +1,67 @@
+
+
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import{MatToolbarModule} from '@angular/material/toolbar';
+
+import { MatSidenavModule } from  '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from  '@angular/material/card';
+
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/template/header/header.component';
+import { FooterComponent } from './components/template/footer/footer.component';
+import { NavComponent } from './components/template/nav/nav.component';
+import { HomeComponent } from './views/home/home.component';
+import { ProductCrudComponent } from './views/product-crud/product-crud.component';
+import { RedDirective } from './directives/red.directive';
+import { ForDirective } from './directives/for.directive';
+import {MatButtonModule} from '@angular/material/button'
+
+
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+
+
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    HomeComponent,
+    ProductCrudComponent,
+    RedDirective,
+    ForDirective
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatCardModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule
+
+
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Projetocrborges/frontend/src/app/directives/for.directive.ts
@@ -0,0 +1,25 @@
+import { Directive, Input, OnInit, TemplateRef, ViewContainerRef } from '@angular/core';
+
+@Directive({
+  selector: '[myFor]'
+})
+export class ForDirective implements OnInit{
+
+  @Input('myForEm') numeros: number[] = [];
+
+
+
+  constructor(private container: ViewContainerRef, private template :TemplateRef<any>) {
+    
+
+    
+   }
+
+   ngOnInit(): void{
+     for (let number of this.numeros ){
+       this.container.createEmbeddedView(this.template, { $implicit: number})
+
+     }
+   }
+
+}
