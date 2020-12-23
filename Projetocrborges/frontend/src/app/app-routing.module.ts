3b22e2b52cb1f2cf8cf1355172d6573a9218d72b
Cristiano Rodrigues Borges <cristianoborges.com@gmail.com>
2020-12-23 18:13:19 -0300

    Retornando os dados do banco

--- Projetocrborges/frontend/src/app/app-routing.module.ts
+++ Projetocrborges/frontend/src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './views/home/home.component';
 import {ProductCrudComponent} from './views/product-crud/product-crud.component';
 import {ProductCreateComponent } from './components/products/product-create/product-create.component';
+import { ProductRead2Component } from './components/products/product-read2/product-read2.component';
 
 const routes: Routes = [
   {path :"" , component : HomeComponent},
   {path :"products" , component : ProductCrudComponent},
-  {path :"products/create" , component : ProductCreateComponent}
+  {path :"products/create" , component : ProductCreateComponent},
+  {path :"products/read2" , component : ProductRead2Component}
 ];
 
 @NgModule({
